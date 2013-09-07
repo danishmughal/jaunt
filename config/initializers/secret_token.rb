9d38d2b7f04f1f6f99aa7ecb7067e72cba150b2b
Danish Mughal <mughal.danish@gmail.com>
2013-09-06 19:46:00 -0700

    base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jaunt::Application.config.secret_key_base = 'eb3866e8a1ab669eb5857731c430c648d602af434069c6c09b7b2f1a01192a418fd76e8555b5554e511e1ce36b09ecf9d670062740b2dc06cb3f575c7f47efa0'
